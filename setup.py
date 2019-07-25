17aef4d2f43522d8caf7a1818ee08e097cd2710a
Marcel Schmalzl <holzkohlengrill@gmail.com>
2019-07-25 23:11:26 +0200

    version bump

--- setup.py
+++ setup.py
@@ -5,10 +5,10 @@ with open("readme.md", "r") as fp:
 
 setuptools.setup(
     name="pypiscout",
-    version=2.3,
+    version=2.4,
     license="MIT",
     author="Marcel Schmalzl",
-    description="Prints formatted console output tagged as info, warning, error, etc. Usage is similar to `print()`.",
+    description="Description Special Character Out (aka pypiscout) | Prints uniformly formatted console output",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/holzkohlengrill/SCout",
