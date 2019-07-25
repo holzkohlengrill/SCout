d9080a5bfef5b40780a95799ca09dd85edcf9598
Marcel Schmalzl <holzkohlengrill@gmail.com>
2019-07-26 00:10:31 +0200

    version bump

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("readme.md", "r") as fp:
 
 setuptools.setup(
     name="pypiscout",
-    version=2.9,
+    version=2.10,
     license="MIT",
     author="Marcel Schmalzl",
     description="Description Special Character Out (aka pypiscout) | Prints uniformly formatted console output",
